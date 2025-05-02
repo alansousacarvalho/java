98c8c53fda20dc11f8c177dfe653df8d5071a925
Alan de Sousa Carvalho <88456324+alansousacarvalho@users.noreply.github.com>
2025-05-02 16:39:00 -0300

    Aplicação da Pipeline com streams

--- lambda/src/application/Program.java
+++ lambda/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program {
@@ -15,16 +16,34 @@ public class Program {
 
 		// Lista para Stream
 
+		/*
+		 * List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+		 * 
+		 * Stream<Integer> st1 = list.stream().map(x -> x * 10);
+		 * System.out.println(Arrays.toString(st1.toArray()));
+		 * 
+		 * Stream<String> st2 = Stream.of("Maria", "Bob", "Alex");
+		 * System.out.println(Arrays.toString(st2.toArray()));
+		 * 
+		 * Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
+		 * System.out.println(Arrays.toString(st3.limit(10).toArray()));
+		 */
+		
+		// Lista para Pipeline
+		
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
-
 		Stream<Integer> st1 = list.stream().map(x -> x * 10);
 		System.out.println(Arrays.toString(st1.toArray()));
 		
-		Stream<String> st2 = Stream.of("Maria", "Bob", "Alex");
-		System.out.println(Arrays.toString(st2.toArray()));
+		int sum = list.stream().reduce(0, (x,y) -> x + y);
+		System.out.println("Sum = " + sum);
 		
-		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
-		System.out.println(Arrays.toString(st3.limit(10).toArray()));
+		List<Integer> newList = list.stream()
+				.filter(x -> x % 2 == 0)
+				.map(x -> x* 10)
+				.collect(Collectors.toList());
+		
+		System.out.println(newList);
 
 		// Predicate
 
