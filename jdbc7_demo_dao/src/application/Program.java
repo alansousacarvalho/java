8382d7c8cc6013d6c4d80af41fa8eb3c98de7391
Alan de Sousa Carvalho <88456324+alansousacarvalho@users.noreply.github.com>
2025-06-05 09:08:32 -0300

    Criação da classe Seller

--- jdbc7_demo_dao/src/application/Program.java
+++ jdbc7_demo_dao/src/application/Program.java
@@ -1,12 +1,21 @@
 package application;
 
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", now, 3000.0, obj);
 		System.out.println(obj);
+		System.out.println(seller);
 	}
-
+	
 }
